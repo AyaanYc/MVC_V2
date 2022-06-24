5593cb38377d399f4f072917f0ff566934fedf7e
CHAN YE <qjdrptmzk@naver.com>
2022-06-24 15:50:45 +0900

    signup

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -9,6 +9,24 @@ class UserController extends Controller {
     }
 
     public function signup() {
-        return "user/signup.php";
+        // if(getMethod() === _GET) {
+        //     return "user/signup.php";
+        // } else if(getMethod() === _POST) {
+        //     return "redirect:signin";
+        // }
+
+        switch(getMethod()) {
+            case _GET:
+                return "user/signup.php";
+            case _POST:
+                $param = [
+                    "email" => $_POST["email"],
+                    "pw" => $_POST["pw"],
+                    "nm" => $_POST["nm"]
+                ];
+                $param["pw"] = password_hash($param["pw"], PASSWORD_BCRYPT);
+                $this->model->insUser($param);
+                return "redirect:signin";
+        }
     }
 }
